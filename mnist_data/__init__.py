3b83bd8decc7064b4325b6f33ba1e6f083423ad7
purelove <594507479@qq.com>
2018-07-25 09:00:54 +0800

    commit first

---
+++ mnist_data/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+__author__ = "purelove"
+__date__ = "2018/7/25 上午8:47"
---
+++ mnist_data/input_data.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = "purelove"
+__date__ = "2018/7/25 上午8:49"
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import gzip
+import os
+import tempfile
+from six.moves import urllib
+
+from six.moves import xrange
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
